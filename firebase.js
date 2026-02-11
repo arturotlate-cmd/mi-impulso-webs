a8f5ae7d12a4bedb6cd65b9fc3da631e8d93a605
Impulso <arturotlate@gmail.com>
2026-02-10 21:38:57 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,16 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-/* CONFIG */
 const firebaseConfig = {
   apiKey: "AIzaSyDS6GpjjaHOfooH6d5SgPQ8PNSReRWc8pI",
   authDomain: "mi-impulso-web.firebaseapp.com",
   projectId: "mi-impulso-web"
 };
 
-/* INIT ÚNICO */
 const app = initializeApp(firebaseConfig);
 
-/* SERVICIOS */
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+
