85af3af9e665c97010bdff4330f31af084669e51
Impulso <arturotlate@gmail.com>
2026-02-10 20:38:21 -0600

    subo archivos para el proyecto

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "impulso-v1";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./styles.css",
+        "./logo-impulso.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
